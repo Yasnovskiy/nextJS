67577b43ec70f8a8006c2b58bf3815481b8a3518
Yasnovsky <andrey5.1997@mail.ru>
2022-11-09 11:37:45 +0500

    test i18nuxt

--- next.config.js
+++ next.config.js
@@ -4,5 +4,6 @@ module.exports = {
   i18n,
   env: {
     SERVER: process.env.SERVER,
+    I18NEXUS_API_KEY: process.env.I18NEXUS_API_KEY,
   },
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,44 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import axios from 'axios';
 import { GetStaticProps } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import Header from '../components/Header/Header';
 
 export default function IndexPage() {
   const router = useRouter();
-  const { locale, defaultLocale } = router;
 
-  const { t } = useTranslation('index');
+  const { locale, defaultLocale } = router;
+  const { t } = useTranslation();
+
+  const [langs, setLang] = useState<any[]>([]);
+  const [nameField, setNameField] = useState<any[]>([]);
 
   console.log('process.env.SERVER ===', process.env.SERVER);
+  console.log('process.env.i18 ===', process.env.I18NEXUS_API_KEY);
+
+  const dfsdfas: any = t('default:bad', { returnObjects: true });
+
+  useEffect(() => {
+    fetch('https://api.i18nexus.com/project_resources/languages.json?api_key=1_MWX5IYYeyHPiOj26kfSA')
+      .then((response) => response.json())
+      .then((data) => {
+        setLang(data.collection);
+        return console.log(data.collection);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch('https://api.i18nexus.com/project_resources/namespaces.json?api_key=1_MWX5IYYeyHPiOj26kfSA')
+      .then((response) => response.json())
+      .then((data) => {
+        setNameField(data.collection);
+        return console.log(data.collection);
+      });
+  }, []);
 
   return (
     <>
@@ -21,22 +47,33 @@ export default function IndexPage() {
 
       <Header />
       <div className="wrapper">
-        <h1>{t('h1')}</h1>
+        <h2>{dfsdfas.title}</h2>
+        <h2>
+          {t('testFolder:testName')}
+        </h2>
+
+        <h2>
+          {t('testFolder:testName')}
+        </h2>
         <p>
-          {t('currentLocale')}
+          {t('testFolder:testBuild')}
           :
           {' '}
           {locale}
         </p>
         <p>
-          {t('defaultLocale')}
+          {t('index:defaultLocale')}
           :
           {' '}
           {defaultLocale}
         </p>
 
+        {
+          langs.map((el) => (<div key={el.full_code}>{el.full_code}</div>))
+        }
+
         <Link href="/contacts">
-          <a>{t('link')}</a>
+          <a>{t('index:link')}</a>
         </Link>
         <br />
       </div>
@@ -44,8 +81,14 @@ export default function IndexPage() {
   );
 }
 
-export const getStaticProps: GetStaticProps = async ({ locale }: any) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ['index'])),
-  },
-});
+export const getStaticProps: GetStaticProps = async ({ locale }: any) => {
+  const { data: products } = await axios.get<any>('https://api.i18nexus.com/project_resources/namespaces.json?api_key=1_MWX5IYYeyHPiOj26kfSA');
+
+  const arr = products.collection.map((el: any) => el.title);
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['index', ...arr])),
+    },
+  };
+};
