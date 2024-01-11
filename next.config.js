484cd9f2b40797ca7c647dfae897ebd4b7f2cf68
Yasnovsky <andrey5.1997@mail.ru>
2024-01-11 13:21:45 +0500

    test: test api-key

--- next.config.js
+++ next.config.js
@@ -6,5 +6,6 @@ module.exports = {
     SERVER: process.env.SERVER,
     SERVER_DEV: process.env.SERVER_DEV,
     I18NEXUS_API_KEY: process.env.I18NEXUS_API_KEY,
+    I18NEXUS_API_KEY_TEST: process.env.I18NEXUS_API_KEY_TEST,
   },
 };
